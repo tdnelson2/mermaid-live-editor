9a7c542e08bf7772ec961e7125ce1dfaf2d1d68a
tdnelson2 <tdnelson@gmail.com>
2023-06-20 15:37:28 -0700

    Add support for node ids within {}, [], (). Description text italicized. Match anywhere (not just beginning of line).

--- src/lib/util/insertIDs.ts
+++ src/lib/util/insertIDs.ts
@@ -1,12 +1,9 @@
-export const insertIDs = (code: string): string => {
-  const lines = code.split(/\r?\n+/);
-  for (let i = 0; i < lines.length; i++) {
-    const parts = lines[i].split('(');
-    if (parts.length > 1) {
-      lines[i] = `${parts[0]}(<u>${parts[0]}</u> ${parts[1]}`;
-    }
-  }
-  console.log(lines.join('\n'));
+function insertSelectedCharacters(str: string): string {
+  const regex = /(\S+)([([{])([^)\]}]+)([)\]}])/g;
+  const result = str.replace(regex, '$1$2$1 <i>$3</i>$4');
+  return result;
+}
 
-  return lines.join('\n');
+export const insertIDs = (code: string): string => {
+  return insertSelectedCharacters(code);
 };
